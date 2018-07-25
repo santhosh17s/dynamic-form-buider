9b9eb6dc8b6dbe929972e2bd7860b4ee9b188bf3
santhosh17s <santhoshloud@gmail.com>
2018-07-25 18:36:16 +0530

    Simple control adding options working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  baseForm: FormGroup;
+  dynamicForm: FormGroup;
+
+  constructor(private _fb: FormBuilder) {}
+
+  ngOnInit() {
+
+     // we will initialize our form here
+     this.baseForm = this._fb.group({
+       name:'Santhosh',
+       email:'santhosh@gmail.com'
+     });
+
+     this.dynamicForm = this._fb.group({})
+
+  }
+
+  addFormControl() {
+    //let name = <FormArray>
+    this.dynamicForm = this._fb.group({
+       name:'',
+       email:''
+    })
+  }
+
+  saveForm(formValue) {
+    console.log(formValue);
+  }
+
+
 }
